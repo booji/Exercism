e8d806d5e7b5fc3f79bf4ae92df1289e5cff1093
booji <pdgcox@gmail.com>
2019-04-18 10:19:48 +0100

    refactor python/matching_brackets

--- python/matching-brackets/matching_brackets.py
+++ python/matching-brackets/matching_brackets.py
@@ -1,38 +1,17 @@
-from collections import deque
-import re
 def is_paired(input_string):
-    braces = 0
-    square = 0
-    paran = 0
-    symbol = deque()
-    for item in re.findall(r"[\(\)\{\}\[\]]",input_string):
-        if item == '(':
-            paran += 1
-            symbol.append(item)
-        elif item == '[':
-            square += 1
-            symbol.append(item)
-        elif item == '{':
-            braces += 1
-            symbol.append(item)
-        else:
+    symbol = []
+    for chr in input_string:
+        if chr in '([{':
+            symbol.append(chr)
+        elif chr in ')]}':
             try:
-                check = symbol.pop()
-            except:
-                return False
-            if item == ')':
-                if check != '(':
-                    return False
-                paran -= 1
-            elif item == ']':
-                if check != '[':
-                    return False
-                square -= 1
-            elif item == '}':
-                if check != '{':
-                    return False
-                braces -= 1
-        if braces < 0 or square < 0 or paran < 0:
-            return False
+                last = symbol.pop()
+                if (chr == ')' and last != '(') or \
+                   (chr == ']' and last != '[') or \
+                   (chr == '}' and last != '{'):
 
-    return braces == 0 and square == 0 and paran == 0
+                    return False
+            except IndexError:
+                return False
+
+    return len(symbol) == 0
